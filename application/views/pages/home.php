729bdc27eed4ed9b5e47dac4941a1c96be61d04d
Kris King <krisking120@gmail.com>
2015-04-19 16:06:09 +1200

    Merge origin/master
    Conflicts:
    	README.md

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,133 +1,136 @@
-<div class="container">
-    <section>
-        <img src="<?php echo base_url(); ?>assets/themes/default/images/header_image.jpg" class="floatL img-responsive" alt="user_gallery"/>
-    </section>
-
-    <div id="body">
-
-        <section class="">
-            <div class="row">
-                <div>
-                    <iframe class="floatL" width="560" height="315" src="https://www.youtube.com/embed/C8rnxVvhoF8" frameborder="0" allowfullscreen></iframe>
-                    <div class="col-md-offset-7">
-                        <h2 class="panel">About</h2>
-                        <br>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui efficitur, lacinia lectus sit amet, consectetur lectus. 
-                            Nullam vel velit est. Suspendisse tempor, sem ut placerat aliquet, nisl nunc congue nulla, 
-                            in suscipit nulla elit at dolor. Donec ultrices lorem lectus, quis efficitur orci hendrerit ac. 
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam tincidunt augue nunc, 
-                            vel elementum elit varius sit amet. Phasellus egestas nunc odio, et eleifend enim rhoncus non. 
-                            Maecenas porttitor urna quam, ut bibendum felis laoreet at.
-                        </p>
-                        <br>
-                        <a class="btn btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                    </div>
-                </div>
+<section id="about">
+    <div class="container element_spacing">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-3">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/Home_Page_logo.png" class="img-responsive" alt="user_gallery"/>
             </div>
-        </section>
-
-        <section>
-            <div class="row">
-                <div>
-                    <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat.jpg" class="floatL img-responsive" alt="user_gallery"/>
-                    <div class="col-md-offset-7">
-                        <h2 class="panel">User Gallery</h2>
-                        <br>
-                        <p>
-                            View some of the images that have been uploaded by other users. 
-                            Only registered users can upload their own images. To become a member 
-                            and upload your own images, all you have to do is create an account.
-                        </p>
-                        <br>
-                        <a class="btn btn-primary" href="<?php echo site_url('site/gallery'); ?>" role="button">View More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                        <a class="btn btn-primary" href="#"  data-toggle="modal" data-target="#imageUploadModal" role="button">
-                            Upload 
-                            <span class="glyphicon glyphicon-upload"></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section>
-
-            <div class="row">
-                <div class="col-md-4">
-                    <h2 class="panel">Airboats</h2>
-                    <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat2.jpg" class="img-responsive" alt="user_gallery"/>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui efficitur, lacinia lectus sit amet, consectetur lectus. 
-                        Nullam vel velit est. Suspendisse tempor, sem ut placerat aliquet. 
-                    </p>
-                    <br>
-                    <a class="btn btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                </div>
-                <div class="col-md-4">
-                    <h2 class="panel">Specifications</h2>
-                    <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat2.jpg" class="img-responsive" alt="user_gallery"/>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui efficitur, lacinia lectus sit amet, consectetur lectus. 
-                        Nullam vel velit est. Suspendisse tempor, sem ut placerat aliquet. 
-                    </p>
-                    <br>
-                    <a class="btn btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                </div>
-                <div class="col-md-4">
-                    <h2 class="panel">Upcoming Events</h2>
-                    <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat2.jpg" class="img-responsive" alt="user_gallery"/>
-                    <br>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui efficitur, lacinia lectus sit amet, consectetur lectus. 
-                        Nullam vel velit est. Suspendisse tempor, sem ut placerat aliquet.
-                    </p>
-                    <br>
-                    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">View More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                </div>
+            <div class="element_spacing col-md-6">
+                <h2 class="text-uppercase">An experience unlike any other</h2>
+                <p class="spacing">
+                  We are passionate about airboats and hope you are too.
+                  It is our mission to provide users the ability to share their experiences with others. 
+                  You can also catch up on upcoming events that are happening in
+                  your region
+                </p>
+                <a class="btn-lg btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn-lg btn-primary" href="#enquiry_form">Make an Enquiry <span class="glyphicon glyphicon-send"></span></a>
             </div>
 
-        </section>
-
-        <section>
-            <div class="row">
-                <div>
-                    <h2 class='panel'>Enquiry</h2>
-                    <p>
-                        Make an enquiry about airboats if you so wish.
-                    </p>
-                    <br>
-                    <div>
-                        <form>
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" class="form-control" id="first_name">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" class="form-control" id="last_name">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="email_address">Email Address</label>
-                                <input type="email" class="form-control" id="email_address">
-                            </div>
-                            <br>
-                            <div class="form-group">
-                                <label for="comment">Comment</label>
-                                <textarea class="form-control" id="comment" rows="8"></textarea>
-                            </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Submit <span class="glyphicon glyphicon-send"></span></button>
-                            <button type="submit" class="btn btn-default">Cancel</button>
-                        </form>
-                    </div>
-                </div>
+            <div class="element_spacing col-md-6">
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/C8rnxVvhoF8" frameborder="0" allowfullscreen></iframe>
             </div>
-        </section>
-        
+        </div>
     </div>
-</div>
+</section>
+
+
+<section id="user_gallery">
+    <div class="container" style="margin-top: 20%;">
+        <div class="row">
+            <div>
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/user_gallery.jpg" class="floatL img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-6">
+                <h2>User Gallery<hr></h2>
+                <p class="spacing">
+                    View some of the images that have been uploaded by other users. 
+                    Only registered users can upload their own images. To become a member 
+                    and upload your own images, all you have to do is create an account.
+                </p>
+                <a class="btn-lg btn-primary" href="<?php echo site_url('site/gallery'); ?>" role="button">View More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn-lg btn-primary" href="#"  data-toggle="modal" data-target="#imageUploadModal" role="button">
+                    Upload 
+                    <span class="glyphicon glyphicon-upload"></span>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section id="content">
+    <div class="container">
+        <div class="row" style="margin-top: 30%; margin-bottom: 20%;">
+            <div class="col-md-4">
+                <h2>Airboats</h2>
+                <hr>
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+                <br>
+                <p>
+                    The River-Crossover Airboat can be used for fishing and hunting, 
+                    sightseeing and adventure tourism, search and rescue, geological exploration, 
+                    military applications, or just having a boatload of fun with family and friends.
+                </p>
+                <br>
+                <a class="btn-lg btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+            <div class="col-md-4">
+                <h2>Specifications</h2>
+                <hr>
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img1.jpg" class="img-responsive" alt="user_gallery"/>
+                <br>
+                <p>
+                    Range from a 4-seat 500kg to a 10-seat 1200kg loading capacity.
+                    Speeds attainable on water depend on the loading of the Airboat, although in general, 
+                    70km/h for the smaller Airboats and up to 110km/h for the larger more powerful Airboats.
+                </p>
+                <br>
+                <a class="btn-lg btn-primary" href="<?php echo site_url('site/about'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+            <div class="col-md-4">
+                <h2>Upcoming Events</h2>
+                <hr>
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+                <br>
+                <p>
+                    There is always something happening with airboats and we can keep you informed when a event is
+                    happening. To be informed of upcoming events, visit our events page to view them.
+                </p>
+                <br>
+                <a class="btn-lg btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">View More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="enquiry_form">
+    <div class="container">
+        <div class="row" style="margin-top: 20%;">
+            <div class="col-xs-12">
+                <h2>Enquiry<hr></h2>
+                <p>
+                    If you wish to make an enquiry about airboats please fill in the form below.
+                </p>
+                <br>
+                <div>
+                    <form>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" class="form-control" id="first_name">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" class="form-control" id="last_name">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="email_address">Email Address</label>
+                            <input type="email" class="form-control" id="email_address">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="comment">Comment</label>
+                            <textarea class="form-control" id="comment" rows="8"></textarea>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Submit <span class="glyphicon glyphicon-send"></span></button>
+                        <button type="submit" class="btn btn-default marg30">Cancel</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
--- application/views/pages/sign_up.php
+++ application/views/pages/sign_up.php
@@ -1,43 +1,50 @@
-<section>
-    <div class="row">
-        <div>
-            <h3>Create an Account</h3>
-            <p>
-                Just fill in the details below and away you go.
-            </p>
-            <br>
+<section class="wrapper">
+    <div class="container">
+        <div class="row">
             <div>
-                <form>
-                    <div class="form-group">
-                        <label for="first_name">First Name</label>
-                        <input type="text" class="form-control" id="first_name">
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="last_name">Last Name</label>
-                        <input type="text" class="form-control" id="last_name">
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="email_address">Email Address</label>
-                        <input type="email" class="form-control" id="email_address">
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password">
-                    </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="confirm_password">Confirm Password</label>
-                        <input type="password" class="form-control" id="confirm_password">
-                    </div>
-                    <br>
-                    <button type="submit" class="btn btn-primary">Submit <span class="glyphicon glyphicon-save"></span></button>
-                    <button type="submit" class="btn btn-default">Cancel</button>
-                </form>
+                <h3>Create an Account<hr></h3>
+                <p>
+                    Just fill in the details below and away you go.
+                </p>
+                <br>
+                <?php echo validation_errors('<div class"alert alert-error">',"</div>"); ?>
+                <div>
+                    <?php echo form_open('auth/create_user'); ?>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" class="form-control" id="first_name" value="<?php echo set_value('first_name'); ?>">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" class="form-control" id="last_name" value="<?php echo set_value('last_name'); ?>">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="email_address">Email Address</label>
+                            <input type="email" class="form-control" id="email_address" value="<?php echo set_value('email_address'); ?>">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" value="<?php echo set_value('password'); ?>">
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="confirm_password">Confirm Password</label>
+                            <input type="password" class="form-control" id="confirm_password" value="<?php echo set_value('password'); ?>">
+                        </div>
+                        <div class="form-group">
+                            <?php echo form_hidden('status', 'Pending'); ?>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Submit <span class="glyphicon glyphicon-floppy-save"></span></button>
+                        <?php echo form_reset('submit', 'Cancel' , 'class="btn btn-default marg15"'); ?>
+                    <?php echo form_close(); ?>
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
+    <br><br>
 </section>
