f221513a8d0cd5475207c046f7e82cad841c366c
Kris King <krisking120@gmail.com>
2015-04-20 11:30:52 +1200

    General HTML/CSS/PHP construction
    Removed some images. Added a new model and controller class. Changed the
    CSS on all pages. Added text and HTML to all pages

--- application/views/pages/events.php
+++ application/views/pages/events.php
@@ -1,67 +1,57 @@
+<section class="wrapper">
+    <section class="container">
+        <h1>Upcoming Events<hr></h1>
+    </section>
+    <section class="container spacing">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+        </div>
+    </section>
 
-<h1>Upcoming Events</h1>
-<br>
-<div>
-    <h2 class="panel">Airboat Meet</h2>
-    <h4>Start Date: 10th June</h4>
-    <h4>Start Date: 12th June</h4>
-    <p>
-        An event is happening and stuff is going down.
-        People who like airboats should come over.
-    </p>
+    <section class="container">
+        <div class="spacing">
+            <h2>Airboat Meet</h2>
+            <h5>Start Date: 10th June</h5>
+            <h5>Start Date: 12th June</h5>
 
-    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <p>
+                An event is happening and stuff is going down.
+                People who like airboats should come over.
+            </p>
 
-</div>
-<br>
-<div>
-    <h2 class="panel">Airboat Meet</h2>
-    <h4>Start Date: 10th June</h4>
-    <h4>Start Date: 12th June</h4>
-    <p>
-        An event is happening and stuff is going down.
-        People who like airboats should come over.
-    </p>
+            <a class="btn-lg btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+        <div class="spacing">
+            <h2>Airboat Meet</h2>
+            <h5>Start Date: 10th June</h5>
+            <h5>Start Date: 12th June</h5>
+            <br>
+            <p>
+                An event is happening and stuff is going down.
+                People who like airboats should come over.
+            </p>
+            <a class="btn-lg btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+        <div class="spacing">
+            <h2>Airboat Meet</h2>
+            <h5>Start Date: 10th June</h5>
+            <h5>Start Date: 12th June</h5>
+            <br>
+            <p>
+                An event is happening and stuff is going down.
+                People who like airboats should come over.
+            </p>
+            <a class="btn-lg btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-</div>
-<br>
-<div>
-    <h2 class="panel">Airboat Meet</h2>
-    <h4>Start Date: 10th June</h4>
-    <h4>Start Date: 12th June</h4>
-    <p>
-        An event is happening and stuff is going down.
-        People who like airboats should come over.
-    </p>
-
-    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-</div>
-<br>
-<div>
-    <h2 class="panel">Airboat Meet</h2>
-    <h4>Start Date: 10th June</h4>
-    <h4>Start Date: 12th June</h4>
-    <p>
-        An event is happening and stuff is going down.
-        People who like airboats should come over.
-    </p>
-
-    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-</div>
-<br>
-<div>
-    <h2 class="panel">Airboat Meet</h2>
-    <h4>Start Date: 10th June</h4>
-    <h4>Start Date: 12th June</h4>
-    <p>
-        An event is happening and stuff is going down.
-        People who like airboats should come over.
-    </p>
-
-    <a class="btn btn-primary" href="<?php echo site_url('site/events'); ?>" role="button">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-</div>
+        </div>
+    </section>
+</section>
--- application/views/pages/gallery.php
+++ application/views/pages/gallery.php
@@ -1,28 +1,34 @@
-<h1>This is example 2</h1>
-<p>Here we have rendered a theme with name "simple" without any external files like css,images,js... e.t.c. </p>
-
-<h2>Code Behind this page</h2>
-<pre style="background: #eee; padding: 10px; border-radius: 5px;">
-&lt?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Example extends CI_Controller {
-
-	function __construct()
-	{
-		parent::__construct();
-
-		$this->load->helper('url');
-
-		$this->_init();
-	}
-
-	...
-
-	public function example_2()
-	{
-		$this->output->set_template('simple');
-		$this->load->view('ci_simplicity/example_2');
-	}
-	...
-}
-</pre>
+<div class="wrapper">
+    <section class="container">
+        <h1 style="margin-bottom: 10%;">User Gallery<hr></h1>
+        <div class="row" style="margin-bottom: 20%;">
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+             <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+            <div class="col-md-4 spacing">
+                <img src="<?php echo base_url(); ?>assets/themes/default/images/airboat_img2.jpg" class="img-responsive" alt="user_gallery"/>
+            </div>
+        </div>
+    </section>
+</div>
